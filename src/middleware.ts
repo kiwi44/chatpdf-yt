09bec141afcf61ee873a306daea7a79514769876
joegardiner <joegardiner@live.com>
2024-10-18 06:02:30 +1000

    initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
-});
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next()
+})
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
+    './app/**/*.{tsnpm audit fix,tsx}',
     './src/**/*.{ts,tsx}',
 	],
   theme: {
